87fce9ea9953c9f37ab3e711421f5be9c8c6c58c
WinsonOTP <102290205+Winson-OTP@users.noreply.github.com>
2022-11-12 15:00:35 +0800

    覆蓋舊版本的檔案

--- evnts/messageCreate.js
+++ evnts/messageCreate.js
@@ -1,15 +1,56 @@
 const { EmbedBuilder } = require('discord.js');
-const cmdCN = require('../cmds/管理指令/channel/commandChannel/main.js')
+const { evaluate } = require('mathjs');
+const cmdCN = require('../cmds/管理指令/channel/commandChannel/main.js');
+const ctCN = require('../cmds/管理指令/channel/countingChannel/main.js');
 
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
+        //指令頻道偵測
         cmdCN.searchByChannelID(message.channel.id, (channel) => {
             if (!channel) return
             message.react('<a:cooldown_60s:1039877691589021707>')
             setTimeout(() => {
                 message.delete()
             }, 60000)
+        })
+        //接龍頻道偵測
+        ctCN.searchByChannelID(message.channel.id, (channel) => {
+            if (!channel) return
+            const formula = message.content.split(' ')[0];
+            let eval = null;
+            try {
+                eval = evaluate(formula)
+                eval = parseInt(eval)
+            } catch(e) {
+                eval = null
+            }
+            let data = channel;
+            if (eval == null) {
+                return
+            } else if (eval != data.record.last.number+1) {
+                let trueNum = data.record.last.number
+                data.record.last.userid = '';
+                data.record.last.number = 0;
+                ctCN.updateChannel(message.guild.id, message.channel.id, data, () => {
+                    message.react('❌')
+                    message.reply(`❌｜正確的數字是 \`${trueNum}\` ，接龍將從 \`1\` 重新開始`);
+                })
+            } else if (message.author.id == data.record.last.userid) {
+                data.record.last.userid = '';
+                data.record.last.number = 0;
+                ctCN.updateChannel(message.guild.id, message.channel.id, data, () => {
+                    message.react('❌')
+                    message.reply('❌｜每位使用者不得連續接兩個以上的數字，數字接龍重新從 `1` 開始');
+                })
+            } else if (eval == data.record.last.number+1) {
+                data.record.last.userid = message.author.id;
+                data.record.last.number = eval;
+                if (eval>data.record.max) data.record.max = eval
+                ctCN.updateChannel(message.guild.id, message.channel.id, data, () => {
+                    message.react('✅')
+                })
+            }
         })
     }
 };
